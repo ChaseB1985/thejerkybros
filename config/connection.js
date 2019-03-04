7a18e3b7f63bf716f3670d5c4313c1ffd81a2c91
chaseb1985 <chasebarrier85@gmail.com>
2019-03-04 10:07:24 -0600

    working on db

--- config/connection.js
+++ config/connection.js
@@ -1,28 +1,33 @@
 let mysql = require ("mysql");
 
-var connection = mysql.createConnection({
-    host    : 'localhost',
-    port    : 3306,
-    user    :'root',
-    password : 'root',
-    database : 'jerky2_db'
-  });
+// var connection = mysql.createConnection({
+//     host    : 'localhost',
+//     port    : 3306,
+//     user    :'root',
+//     password : 'root',
+//     database : 'jerky2_db'
+//   });
 
-  connection.connect(function(err) {
-    if (err) {
-        console.error("error connecting: " +err.stack);
-        return;
-    }
-    console.log("hey hey you connected as id " + connection.threadId);
-});
-    // connection.connect(function(err){
-    //   if (err) throw err;
-    //   console.log('hey you connected');
-    //   let sql = `CREATE DATABASE jerky2_db`;
-    //   connection.query(sql, function (err, res){
-    //     if (err) throw err;
-    //     console.log('DB created');
-    //   });
-    // });
+//   connection.connect(function(err) {
+//     if (err) {
+//         console.error("error connecting: " +err.stack);
+//         return;
+//     }
+//     console.log("hey hey you connected as id " + connection.threadId);
+// });
+var connection;
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'jerky2_db'
+    })
+};
 
 module.exports = connection;
